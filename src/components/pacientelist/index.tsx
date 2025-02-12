6867ea2837e4c0dff8c708febec01bc748002724
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2025-02-12 13:52:10 -0700

    Enhance PacienteList component with scrollable view and minor text adjustment

--- src/components/pacientelist/index.tsx
+++ src/components/pacientelist/index.tsx
@@ -6,7 +6,7 @@ function PacienteList() {
   const { pacientes } = usePacienteStore()
   console.log(pacientes)
   return (
-    <div className='md:w-1/2 lg:w-3/5 mx-5'>
+    <div className='md:w-1/2 lg:w-3/5 mx-5 md:h-screen overflow-y-scroll'>
       {
         pacientes.length === 0 ? (
           <>
@@ -22,7 +22,7 @@ function PacienteList() {
           <>
             <h2 className='font-black text-3xl text-center'>Listado de pacientes</h2>
             <p className='text-xl mt-5 mb-10 text-center'>
-              Administra tus {''}
+              Administra tus {' '}
               <span className='text-indigo-600 font-bold'>
                 Pacientes y Citas
               </span>
